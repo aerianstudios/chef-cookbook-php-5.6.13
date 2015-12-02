21e0a70c4844b96a174feb13b3b5742407b303d1
Ben <RaringRingtail>
2015-12-02 13:50:05 +0000

    Added enable_mycrypt and removed disable_xdebug

--- recipes/enable_mcrypt.rb
+++ recipes/enable_mcrypt.rb
@@ -1,11 +1,6 @@
-if node[:php]
-  
-  # With bash
-  bash "insert_line" do
-    user "root"
-    code <<-EOS
-    sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php5/apache2/php.ini
-    EOS
-  end
-  
+bash "enable_mycrypt" do
+  user "root"
+  code <<-EOS
+    php5enmod mcrypt
+  EOS
 end

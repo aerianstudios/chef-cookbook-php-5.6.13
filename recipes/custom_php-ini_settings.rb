49010494196d574dd88b26e7817eb5e98fed3f6c
Ben <RaringRingtail>
2015-12-02 14:15:40 +0000

    Added custom php.ini settings

--- recipes/custom_php-ini_settings.rb
+++ recipes/custom_php-ini_settings.rb
@@ -1,11 +1,10 @@
-if node[:php]
-  
-  # With bash
-  bash "insert_line" do
-    user "root"
-    code <<-EOS
+bash "custom_php-ini_settings" do
+  user "root"
+  code <<-EOS
     sed -i "s/short_open_tag = Off/short_open_tag = On/" /etc/php5/apache2/php.ini
-    EOS
-  end
-  
+    sed -i "s/max_input_time = 60/max_input_time = 600/" /etc/php5/apache2/php.ini
+    sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 100M/" /etc/php5/apache2/php.ini
+    sed -i "s/post_max_size = 8M/post_max_size = 100M/" /etc/php5/apache2/php.ini
+    sed -i "s/date.timezone = /date.timezone = Europe/London/" /etc/php5/apache2/php.ini
+  EOS
 end
